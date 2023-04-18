a643c9d683937100bcfb2bedf95051b016873a4d
david_tsai <108356037@nccu.edu.tw>
2023-04-19 01:23:22 +0800

    update deploy script

--- script/session-4-bundler-demo/DeployAccount.s.sol
+++ script/session-4-bundler-demo/DeployAccount.s.sol
@@ -13,7 +13,11 @@ contract DeployAccount is Script {
         vm.startBroadcast(deployer);
 
         NonStandardAccount account = new NonStandardAccount(
+            // StackUp bundler entryPoint
+            // IEntryPoint(0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789),
             IEntryPoint(0x0576a174D229E3cFA37253523E645A78A0C91B57),
+            // This adddress is the owner of wallet account,
+            // replace an EOA account that is under your control (e.g. you have its private key)
             0xE82493656F31dAC3c839fa93449Ee4F21Fdd05b7
         );
 
