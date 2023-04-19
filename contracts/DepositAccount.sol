97a902469604052ff5438e2d2a30122cbe728706
pilagod <pilagooood@gmail.com>
2023-04-19 13:35:49 +0800

    refine comments

--- contracts/DepositAccount.sol
+++ contracts/DepositAccount.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-/* solhint-disable no-unused-vars */
+/* solhint-disable no-inline-assembly no-unused-vars */
 pragma solidity 0.8.17;
 
 import { IAccount } from "aa/interfaces/IAccount.sol";
@@ -18,14 +18,19 @@ contract DepositAccount is IAccount {
         uint256 missingAccountFunds
     ) external returns (uint256 validationData) {
         // TODO: Implement this method to pass the tests in test/DepositAccount.t.sol
+        //
         // Account should deposit missing funds to EntryPoint to pay gas for the user operation.
+        // 
+        // There are two possible ways to achieve this goal:
+        // (1) Send ETH directly to EntryPoint 
+        // (2) Call `deposit` on EntryPoint
+
         return 0;
     }
 
     function execute(address target, uint256 value, bytes calldata data) external {
         (bool success, bytes memory result) = target.call{ value: value }(data);
         if (!success) {
-            /* solhint-disable no-inline-assembly */
             assembly {
                 revert(add(result, 32), mload(result))
             }
