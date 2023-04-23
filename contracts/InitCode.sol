4f12f2d2f066fa1d57169f36ca99e0e546789921
pilagod <pilagooood@gmail.com>
2023-04-24 00:10:51 +0800

    refine encoding

--- contracts/InitCode.sol
+++ contracts/InitCode.sol
@@ -46,7 +46,7 @@ contract SignatureAccountFactory {
         return
             Create2.computeAddress(
                 bytes32(salt),
-                keccak256(abi.encodePacked(type(SignatureAccount).creationCode, abi.encode(entryPoint), abi.encode(owner)))
+                keccak256(abi.encodePacked(type(SignatureAccount).creationCode, abi.encode(entryPoint, owner)))
             );
     }
 }
